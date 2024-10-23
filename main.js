8844749b301cf55513df591db9abe93bdf9f4ba6
vinnietito <vincentnjuguna69@gmail.com>
2024-10-23 23:36:27 +0300

    Second Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+anime({
+    targets:'div.box.red',
+    translateY:[
+    {value:200,duration:500},
+    {value:0,duration:800},
+    ],
+    rotate:{
+    value:'1turn',
+    easing:'easeInOutSine',
+    }
+    });
